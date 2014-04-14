d6e440a445c8a318e97c35e39bd1ce1edc9f52c4
Francois Suter <francois@typo3.org>
2014-04-14 15:20:56 +0200

    [TASK] Verify compatibility with TYPO3 CMS 6.2
    Nothing to do :-)
    
    Resolves: #56130
    Releases: 2.3
    Change-Id: I587ddaa811fb0bd2ea70e2fdad8ef0a336b58003
    Reviewed-on: https://review.typo3.org/29454
    Reviewed-by: Francois Suter
    Tested-by: Francois Suter

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,21 +28,21 @@ $EM_CONF[$_EXTKEY] = array (
   'lockType' => '',
   'author_company' => '',
   'version' => '2.2.1',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
-      'typo3' => '4.5.0-6.1.99',
+      'typo3' => '4.5.0-6.2.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
   '_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"6aac";s:21:"class.tx_overlays.php";s:4:"192f";s:16:"ext_autoload.php";s:4:"ad6d";s:12:"ext_icon.gif";s:4:"73e0";s:10:"README.txt";s:4:"b729";s:14:"doc/manual.pdf";s:4:"f295";s:14:"doc/manual.sxw";s:4:"622c";}',
-  'suggests' => 
+  'suggests' =>
   array (
   ),
   'comment' => 'Fixed bug in handling of l10n_mode with TYPO3 CMS 6.1',
